56a3b44cf653e0f4562886ab37e3ea1b11cb405d
Joshua Small <jsmall17@uncc.edu>
2018-12-23 12:39:35 -0500

    stripe bug fix

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,21 @@
+class SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+    customer = current_user.stripe_customer
+
+    subscription = customer.subscriptions.create(
+      source: params[:stripeToken],
+      plan: params[:plan]
+    )
+
+    current_user.assign_attributes(stripe_subscription_id: subscription.id)
+    current_user.save
+
+    flash.notice = "Thanks for subscribing!"
+    redirect_to root_path
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def stripe_customer
+    if stripe_id?
+      Stripe::Customer.retrieve(stripe_id)
+    else
+      stripe_customer = Stripe::Customer.create(email: email)
+      update(stripe_id: stripe_customer.id)
+      stripe_customer
+    end
+  end
+end
