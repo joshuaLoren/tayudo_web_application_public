682994c15c89e863f35d876eae8cf899373037cb
EC2 Default User <ec2-user@ip-172-31-45-202.us-east-2.compute.internal>
2018-12-23 18:29:31 +0000

    stripe bug fixed, customers can subsribe to beta

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,6 +7,14 @@ class SubscriptionsController < ApplicationController
   def create
     customer = current_user.stripe_customer
 
-    render action: :new
+    customer = Stripe::Customer.create
+    customer.sources.create(source: params[:stripeToken])
+    subscription = customer.subscriptions.create(plan: params[:plan])
+
+    current_user.assign_attributes(stripe_subscription_id: subscription.id)
+    current_user.save
+
+    flash.notice = "Thanks for subscribing!"
+    redirect_to root_path
   end
 end
